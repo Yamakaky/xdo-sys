aaac1b44349c39fcf1c4b6e82866ec1e504b28bf
Mikaël Fourrier <mikael.fourrier@pm.me>
2021-08-24 22:30:18 +0200

    features!!!

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,10 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+#[cfg(feature = "generate")]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(not(feature = "generate"))]
+mod bundled;
+#[cfg(not(feature = "generate"))]
+pub use bundled::*;
